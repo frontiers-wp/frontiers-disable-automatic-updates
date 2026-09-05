77dad8ee4b7ddcfad48988912d704fef9a5c9ca5
frontiers <wordpress@bekamhealing.com>
2026-09-05 14:59:23 +0200

    Update frontiers-disable-automatic-updates.php
    1.0.1

--- frontiers-disable-automatic-updates.php
+++ frontiers-disable-automatic-updates.php
@@ -9,35 +9,34 @@ defined('ABSPATH') or die('Direct access not permitted.');
  * Author: Edwin Bekedam
  * Author URI: https://github.com/frontiers-wp/frontiers-disable-automatic-updates
  * Donate link: https://paypal.me/EBekedam
- * Version: 1.0.0
+ * Version: 1.0.1
  * License: GPL-2.0+
  */
 
 // Define constant with current version
 if (! defined( 'FRONTIERS_DISABLE_AUTOMATIC_UPDATES_VERSION' ) ) {
-    define( 'FRONTIERS_DISABLE_AUTOMATIC_UPDATES_VERSION', '1.0.0' );
+    define( 'FRONTIERS_DISABLE_AUTOMATIC_UPDATES_VERSION', '1.0.1' );
 }
 
 /**
  * Disables WordPress automatic updates for plugins, themes, and core.
- * Hooks into WordPress filters to prevent automatic updates.
+ * Runs instantly upon file load to intercept background cron processes.
  */
 function frontiers_disable_automatic_updates() 
 {
     // Prevent all plugin updates from being automatically installed
     add_filter('auto_update_plugin', '__return_false');
-    // Disable plugins auto-update email notifications.
-    add_filter( 'auto_plugin_update_send_email', '__return_false' );
+    add_filter('auto_plugin_update_send_email', '__return_false');
 
     // Prevent all theme updates from being automatically installed
     add_filter('auto_update_theme', '__return_false');
     add_filter('core_update_skip_new_bundled', '__return_true');
-    // Disable themes auto-update email notifications.
-	add_filter('auto_theme_update_send_email', '__return_false' );
+    add_filter('auto_theme_update_send_email', '__return_false');
 
     // Prevent WordPress core from automatically updating
-	add_filter('auto_update_core', '__return_true');
+    add_filter('auto_update_core', '__return_false');
     add_filter('wp_auto_update_core', '__return_false');
+    add_filter('auto_core_update_send_email', '__return_false');
 }
 
 /**
@@ -51,8 +50,5 @@ function frontiers_disable_automatic_core_updates()
     }
 }
 
-// Hook the main update disable function to WordPress initialization
-add_action('init', 'FrontiersDisableAllAutomatic\frontiers_disable_automatic_updates');
-
-// Hook the core update disable function to admin initialization
-add_action('admin_init', 'FrontiersDisableAllAutomatic\frontiers_disable_automatic_core_updates');
+// Execute immediately on file load so background cron runs capture the filters
+frontiers_disable_automatic_updates();
